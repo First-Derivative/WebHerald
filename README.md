09bcfe586895cd0fec6c65b00bf97519266c00b3
Ashraff Hatz <ashraff.hatz@gmail.com>
2020-11-13 18:11:15 +0000

    update main Models

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # WP_Coursework3
 
+---
+## Important Links
 ### [For Merge Conflict Reference](https://www.youtube.com/watch?v=CKAdoAR0ykc&ab_channel=AppleJuiceTeaching)
+
+---
+## Note for Peer:
+
+We need to discuss the impact of adding db.sqlite3 to the .gitignore file for localized db versions.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Article(models.Model):
+    '''
+    Class that implements the strong entity 'Article'. Holds data pertaining
+    to an article on the web application.
+    '''
+    # need implementation
+
+class Comments(models.Model):
+    '''
+    Class that implements the associative entity 'Comments'. Intended to keep track
+    of which User posted what comment_content on which Article. Furthermore a comment foreign key
+    may be held in order to discern if a comment is a reply to a parent comment or a general for
+    an Article.
+    '''
+    comment_content = models.CharField(max_length=300)
+    timestamp = models.DateTimeField(verbose_name='Commented On', auto_now=True) # verbose_name acts as a more legible alias for the field name. auto_now means that if the date value is not given upon object creation then if you save said object it will automatically take the current date and time as the value upon Model.save().
+    parent_comment = models.ForeignKey('Comments', blank=True, null=True, on_delete=models.CASCADE)
+
+    @property
+    def hasParent(self):
+        if parent_comment:
+            return True
+        return False
