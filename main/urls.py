9ce54da5df49f1f0e6591d213becb09074c4072c
Nicole Robson <nicolesrobson@gmail.com>
2020-11-15 18:45:11 +0000

    added article template and basic url

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import indexView
+from main.views import indexView, articleView
 
 urlpatterns = [
-    path('', indexView),
+    path('', indexView, name='index'),
+    path('/article/',  articleView, name='article'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Serve up the homepage
 def indexView(request):
     return render(request, 'main/index.html')
+
+# Serve article page, hardcoded for now
+def articleView(request):
+    return render(request, 'main/article.html')
